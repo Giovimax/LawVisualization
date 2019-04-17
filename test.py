5f4fe1f5be5b6b3ee4814b9a49c92facfb358334
Giovimax <giovimax@gmail.com>
2019-04-17 18:59:04 +0200

    deleted useless parts and update the rawtopathdict part

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ Created on Wed Apr 10 13:44:06 2019
 import bs4 as bs
 import pandas as pd 
 import requests as re
-from LawVis import *
+import LawVis as lv
 import networkx as nx 
 import pickle 
 from os import listdir
@@ -22,7 +22,7 @@ from time import sleep
     The code can function with the while: and the flag "status" or with the 
     for loop with the range funct if only a fraction of the articles is needed.
 """
-if "all.txt" not in listdir():
+if "Data/all.txt" not in listdir():
     print("no all.txt, creating and dumping")
     #safety mesure
     try:
@@ -35,7 +35,7 @@ if "all.txt" not in listdir():
     
     while status:
 #    for i in range(10):
-        item = crawlBoccardi(link[-1],raw = False, complex_=True)
+        item = lv.crawlBoccardi(link[-1],raw = False, complex_=True)
         sleep(0.1)
         todf.append(item[0])
         if item[1] == False:
@@ -59,26 +59,48 @@ else:
 
 
 #extraction of the dict from raw to path
-rawToPatDict = dict()
-"""A dict that contains the particle names from the link as keys and the 
-name extracted from the path in the website as value, it is needed to map 
-more readeable names over the raw ones"""
-for i in range(len(df)):#for row in df
-    path, link = df.loc[i,["path","link"]]#unpacks and assigns 
-    #preparation 
-    #cl
-    cl = clearLink(link)
-    for n, i in enumerate(cl):
-        if i == 'www.brocardi.it': #removes everything before and the domain 
-            cl = cl[n+1:]
-            break
-    #path
-    path = path[1:] #removes the useless part
-    #main
-    for k,v in zip(cl,path): 
-        if k not in rawToPatDict:
-            rawToPatDict[k] = v
-    
+#rawToPatDict = dict()
+#"""A dict that contains the particle names from the link as keys and the 
+#name extracted from the path in the website as value, it is needed to map 
+#more readeable names over the raw ones"""
+#for i in range(len(df)):#for row in df
+#    path, link = df.loc[i,["path","link"]]#unpacks and assigns 
+#    #preparation 
+#    #cl
+#    cl = lv.clearLink(link)
+#    for n, i in enumerate(cl):
+#        if i == 'www.brocardi.it': #removes everything before and the domain 
+#            cl = cl[n+1:]
+#            break
+#    #path
+#    path = path[1:] #removes the useless part
+#    #main
+#    for k,v in zip(cl,path): 
+#        if k not in rawToPatDict:
+#            rawToPatDict[k] = v
+        # ^ depreciated by genRawToPathDict
+
+def genRawToPathDict(df):
+    """take the df for one code and combines the well formatted names of 
+    the code's sections from the page and the paths from the link"""
+    toret = dict()
+    for i in range(len(df)):#for row in df
+        path, link = df.loc[i,["path","link"]]#unpacks and assigns 
+        #preparation 
+        #cl
+        cl = lv.clearLink(link)#stands for clear link
+        for n, i in enumerate(cl):
+            if i == 'www.brocardi.it': #removes everything before and the domain 
+                cl = cl[n+1:]
+                break
+        #path
+        path = path[1:] #removes the useless part
+        #main
+        for k,v in zip(cl,path): 
+            if k not in toret:
+                toret[k] = v
+    return toret
+
 def rawToPath(rawlist): #uses the rawToPatDict dict to substitute matching vals
     rawlistcopy = list(rawlist)
     for i, l in enumerate(rawlist):
@@ -91,46 +113,10 @@ def rawToPath(rawlist): #uses the rawToPatDict dict to substitute matching vals
 #%%
 #CREATION OF THE NETWORK OBJECT
 g = nx.Graph()
-#Function that creates additional links (DEPRECIATED)
-
-#for nitem, item in enumerate(df["link_commi"]):
-#    item_name = df["path"][nitem][-1]
-#    for link in item:
-#        link = clearLink(link)
-#        if link[0] =='codice-civile':
-#            for n,j in enumerate(link):
-#                if j not in g.nodes:
-#                    g.add_node(j)
-#                    if n !=len(link)-1:
-#                        g.add_edge(j,link[n-1])
-#                    else:
-#                        g.add_edge(j,item_name)
-#        else:
-#            link = link[::-1]
-#            for n, j in enumerate(link):
-#                if n == 0:
-#                    g.add_edge(j,item_name)
-#                else:
-#                    g.add_edge(j,link[n-1])
-#                    
 
 #%%
-
-#g.add_nodes_from([str(i) for i in df["path"]])
-
-#CREATION OF THE GRAPH
-#method with path
-
-#for i in df["path"]:
-#    for n,j in enumerate(i):
-#        if j not in g.nodes:
-#            g.add_node(j)
-#            if n !=0:
-#                g.add_edge(j,i[n-1])
-#            else:
-#                pass
-#        else:
-#            pass
+rawToPatDict = genRawToPathDict(df)
+#%%
 
 # FIXME: ?
 # XXX: ?
@@ -140,9 +126,9 @@ g = nx.Graph()
 
 #advanced method with function
 # TODO: make rawToPatDict independent 
-def addItemAsNode(G,link):
+def addItemAsNode(G,link,ToPatDict):
     #TODO: change link to the list from path_from_dict to diminish processing
-    clearLinkList = clearLink(link)#list of relevent items
+    clearLinkList = lv.clearLink(link)#list of relevent items
     
     iterable = []
     """i'm now using as a univoque node the full relevant path of each section 
@@ -155,8 +141,8 @@ def addItemAsNode(G,link):
         if node not in G.nodes:
             #name part
             name = None
-            if node[-1] in rawToPatDict: #sets the name as pretty name if possible
-                name = rawToPatDict[node[-1]]
+            if node[-1] in ToPatDict: #sets the name as pretty name if possible
+                name = ToPatDict[node[-1]]
             #adding node
             G.add_node(node,name=name)
             #creating edges
@@ -169,18 +155,6 @@ def addItemAsNode(G,link):
 for link in df["link"]:
     addItemAsNode(g,link)
    
-##method with link (DOES NOT WORK PROPERLY)
-#for i in df["link"]:
-#    l = i.split("/")[3:]
-#    l[-1] = l[-1].split(".")[0]
-#    for n,j in enumerate(l):
-#        if j not in g.nodes:
-#            g.add_node(j)
-#            if n !=0:
-#                g.add_edge(j,i[n-1])
-#            else:
-#                pass
-#        else:
-#            pass
+
 #%%
 nx.draw(g, with_labels=True)
